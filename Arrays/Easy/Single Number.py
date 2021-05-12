452ac8fb656a49acea5dc2f291072b072f848a70
Palash Sharma <palashsharma891@gmail.com>
2021-05-12 07:46:20 +0530

    Update Single Number.py

--- Arrays/Easy/Single Number.py
+++ Arrays/Easy/Single Number.py
@@ -1,5 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        #hashmap
+        #math -> 2 * (a+b+c) - (a+a+b+b+c) = c
+        #bitmasking -> a^0 = a and a^a = 0; a^b^a = a^a^b = 0^b = b
         a = 0
         for i in nums:
             a ^= i
